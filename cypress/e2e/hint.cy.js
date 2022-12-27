a4d0634b39243dfaf9d0a0ec2efc5150a5f23fd3
abouman76 <abouman76@gmail.com>
2022-12-27 15:13:02 +0100

    hint spec done

---
+++ cypress/e2e/hint.cy.js
@@ -0,0 +1,21 @@
+describe("Hint", () => {
+    beforeEach(() => {
+        cy.visit('/')
+    });
+    it('should fill all empty cells and solv the Sudoku', () => {
+        cy.get('.game__cell.game__cell--filled')
+            .should('have.length', 45);
+
+        cy.get('.game__cell')
+            .not('.game__cell--filled')
+            .each($cell => {
+                cy.wrap($cell)
+                    .click();
+                cy.get('.status__action-hint')
+                    .click();
+            });
+        cy.get('.overlay__text')
+            .should('have.text', 'You solved it!')
+            .and('be.visible');
+    });
+});
